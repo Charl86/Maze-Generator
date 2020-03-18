6e40b16ad3237d706846f9ada5db850e01542f12
Carlos Hernandez <carl.hernan34@gmail.com>
2020-03-18 13:52:22 -0400

    Made some changes to figure out the padding problem.

--- border.py
+++ border.py
@@ -1,6 +1,6 @@
 import pygame
 import pygame.gfxdraw
-from config import Vars, Colors, PygameVars as Pyv
+from config import Colors, PygameVars as Pyv
 
 
 class Border:
@@ -9,6 +9,11 @@ class Border:
     def __init__(self, x, y):
         self.x = x
         self.y = y
+
+        self.top_left_corn = self.x, self.y
+        self.bot_left_corn = self.x, Pyv.HEIGHT - 2 * self.y - 6
+        self.top_right_corn = Pyv.WIDTH - 2 * self.x - 6, self.y
+        self.bot_right_corn = Pyv.WIDTH - 2 * self.x - 6, Pyv.HEIGHT - 2 * self.y - 6
 
         self.color = Colors.RED  # The color of the border.
         self.thickness = 2  # The thickness of the border.
@@ -19,11 +24,12 @@ class Border:
 
     def draw(self):
         if self.x or self.y:
-            # pygame.draw.rect(Pyv.SCREEN, self.color, pygame.Rect(
-            #     (self.x, self.y), (Pyv.WIDTH - 2 * self.x, Pyv.HEIGHT - 2 * self.y)),
-            #                  self.thickness)
+            pygame.draw.rect(Pyv.SCREEN, self.color, pygame.Rect(
+                (self.x, self.y), (390, 390)),
+                             self.thickness)
+
             # There's a constant '6'
             # TODO: Generalize the constant
-            pygame.draw.rect(Pyv.SCREEN, self.color, pygame.Rect(
-                (self.x, self.y), (Pyv.WIDTH - 2 * self.x - 6, Pyv.HEIGHT - 2 * self.y - 6)),
-                             self.thickness)
+            # pygame.draw.rect(Pyv.SCREEN, self.color, pygame.Rect(
+            #     (self.x, self.y), (Pyv.WIDTH - 2 * self.x - 6, Pyv.HEIGHT - 2 * self.y - 6)),
+            #                  self.thickness)
