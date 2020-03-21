7046917a6782018a7c859d22e9877a611c3eb253
Carlos Hernandez <carl.hernan34@gmail.com>
2020-03-21 17:10:09 -0400

    Modified the Tkinter_Setup.py to adjust the position of objects in the tkinter window. Started to implement the 'padding_func()' function.

--- maze_tkinter/Tkinter_Setup.py
+++ maze_tkinter/Tkinter_Setup.py
@@ -16,6 +16,7 @@ class TkinterWindow:
         self.num_of_rows = tk.StringVar()  # Variable for future entry object value.
         self.info_label_text = tk.StringVar()  # Variable for future label object value.
         self.cell_size = tk.StringVar()  # Variable for future entry object value.
+        self.suggested_text_val = tk.StringVar()  # Variable for future label object.
 
         # Create a frame object in master.
         self.frame_1 = ttk.Frame(self.master, padding=(10, 10, 10, 10), width=400, height=400)
@@ -29,14 +30,21 @@ class TkinterWindow:
         # Create a label object within frame_1 with value "Cell size:".
         self.cell_size_text = ttk.Label(self.frame_1, text="Cell size:")
         # Create a label object within frame_1 with value the same as the info_label_text variable.
-        self.validation_text = ttk.Label(self.frame_1, textvariable=self.info_label_text, wraplength=150)
+        # self.validation_text = ttk.Label(self.frame_1, textvariable=self.info_label_text, wraplength=150)
+        self.validation_text = ttk.Label(self.frame_1, textvariable=self.info_label_text, wraplength=225)
+
+        # Create a label object within frame_1 with value the same as suggested_text_val.
+        self.suggestion_label = ttk.Label(self.frame_1, textvariable=self.suggested_text_val)
 
         # Create an entry object with the value of the num_of_cols variable.
-        self.rows_entry = ttk.Entry(self.frame_1, width=2, textvariable=self.num_of_cols)
+        # self.rows_entry = ttk.Entry(self.frame_1, width=1, textvariable=self.num_of_cols)
+        self.rows_entry = ttk.Entry(self.frame_1, width=5, textvariable=self.num_of_cols)
         # Create an entry object with the value of the num_of_rows variable.
-        self.cols_entry = ttk.Entry(self.frame_1, width=2, textvariable=self.num_of_rows)
+        # self.cols_entry = ttk.Entry(self.frame_1, width=1, textvariable=self.num_of_rows)
+        self.cols_entry = ttk.Entry(self.frame_1, width=5, textvariable=self.num_of_rows)
         # Create an entry object with the value of the cell_size variable.
-        self.cell_size_entry = ttk.Entry(self.frame_1, width=2, textvariable=self.cell_size)
+        # self.cell_size_entry = ttk.Entry(self.frame_1, width=1, textvariable=self.cell_size)
+        self.cell_size_entry = ttk.Entry(self.frame_1, width=5, textvariable=self.cell_size)
 
         # Create the button object bind to the method generate().
         self.gene_butt = ttk.Button(self.frame_1, text="Generate", command=self.generate)
@@ -44,34 +52,52 @@ class TkinterWindow:
         # Griding:
         self.frame_1.grid(column=0, row=0, sticky=(tk.N, tk.W, tk.E, tk.S))
 
-        self.title_text.grid(column=0, row=0, pady=15, columnspan=4)
-        self.validation_text.grid(column=2, row=1, rowspan=2, padx=20)
+        # self.title_text.grid(column=0, row=0, pady=15, columnspan=4)
+        self.title_text.grid(column=0, row=0, pady=15, columnspan=7)
+        # self.validation_text.grid(column=2, row=1, rowspan=2, padx=20)
+        self.validation_text.grid(column=3, columnspan=3, row=1, rowspan=3, padx=[10, 0],
+                                  sticky=(tk.W, tk.N, tk.W, tk.S))
 
-        self.nums_rows_text.grid(column=0, row=1, sticky=tk.W, padx=10)
-        self.nums_cols_text.grid(column=0, row=2, sticky=tk.W, padx=10)
-        self.cell_size_text.grid(column=0, row=3, sticky=tk.W, padx=10, pady=4)
-        self.rows_entry.grid(column=1, row=1, sticky=(tk.W, tk.E))
-        self.cols_entry.grid(column=1, row=2, sticky=(tk.W, tk.E))
-        self.cell_size_entry.grid(column=1, row=3, sticky=(tk.W, tk.E))
+        self.suggestion_label.grid(column=1, columnspan=2, row=4, sticky=(tk.W, tk.E), padx=[13, 0], pady=3)
 
-        self.gene_butt.grid(column=3, row=1, sticky=tk.E)
+        # self.nums_rows_text.grid(column=0, row=1, sticky=tk.W, padx=10)
+        # self.nums_cols_text.grid(column=0, row=2, sticky=tk.W, padx=10)
+        # self.cell_size_text.grid(column=0, row=3, sticky=tk.W, padx=10, pady=4)
+
+        self.nums_rows_text.grid(column=0, row=1, sticky=tk.W)
+        self.nums_cols_text.grid(column=0, row=2, sticky=tk.W)
+        self.cell_size_text.grid(column=0, row=3, sticky=tk.W)
+
+        # self.rows_entry.grid(column=1, row=1, sticky=(tk.W, tk.E))
+        # self.cols_entry.grid(column=1, row=2, sticky=(tk.W, tk.E))
+        # self.cell_size_entry.grid(column=1, row=3, sticky=(tk.W, tk.E))
+
+        self.rows_entry.grid(column=1, row=1, sticky=(tk.W, tk.E), padx=[15, 0])
+        self.cols_entry.grid(column=1, row=2, sticky=(tk.W, tk.E), padx=[15, 0])
+        self.cell_size_entry.grid(column=1, row=3, sticky=(tk.W, tk.E), padx=[15, 0], pady=[3, 0])
+
+        # self.gene_butt.grid(column=3, row=1, sticky=tk.E)
+        self.gene_butt.grid(column=6, row=1)
 
         # Column & Row configure:
         self.master.columnconfigure(0, weight=1)
         self.master.rowconfigure(0, weight=1)
 
-        self.frame_1.columnconfigure(1, weight=2)
+        self.frame_1.columnconfigure(1, weight=3)
         self.frame_1.columnconfigure(2, weight=3)
         self.frame_1.columnconfigure(3, weight=3)
+        self.frame_1.columnconfigure(4, weight=3)
+        self.frame_1.columnconfigure(5, weight=3)
 
         self.frame_1.rowconfigure(0, weight=1)
         self.frame_1.rowconfigure(1, weight=1)
-
         self.frame_1.rowconfigure(2, weight=1)
         self.frame_1.rowconfigure(3, weight=1)
+        self.frame_1.rowconfigure(4, weight=1)
 
-        # Column & Row configure: Size
-        self.master.columnconfigure(1, minsize=2)
+        # self.master.columnconfigure(3, minsize=20)
+        # self.master.columnconfigure(4, minsize=20)
+        # self.master.columnconfigure(5, minsize=20)
 
         # Bindings:
         self.rows_entry.bind("<Return>", self.generate)
@@ -81,15 +107,15 @@ class TkinterWindow:
             self.gene_butt.bind("<1>", self.generate)
             self.gene_butt.bind("<Return>", self.generate)
 
+        # Bind the event of the close button being press to the self.on_closing() method.
         self.master.protocol("WM_DELETE_WINDOW", self.on_closing)
 
         # Every self.delay milliseconds, apply the self.enable_button() method.
         self.master.after(self.delay, self.enable_button)
-        # self.master.minsize(456, 131)
-        # self.master.maxsize(1200, 1200)
 
-        # Minimum size of the Tkinter window.
-        self.master.minsize(500, 131)
+        # Set the minimum size of the Tkinter window.
+        # self.master.minsize(548, 142)
+        self.master.minsize(548, 163)
 
     def generate(self, *args):
         # If the state of the generation button is normal and the button was pressed
@@ -99,6 +125,8 @@ class TkinterWindow:
             self.master.destroy()  # kill the window.
 
     def enable_button(self):
+        Debugger.STREAMER.debug(self.get_wind_size())
+
         # Get the values of the entries and store them in the given variables.
         col_entry, row_entry = self.num_of_cols.get(), self.num_of_rows.get()
         cell_size_number = self.cell_size_entry.get()
@@ -113,25 +141,34 @@ class TkinterWindow:
         elif not col_entry.isdigit() or not row_entry.isdigit() or not cell_size_number.isdigit():
             # display a message stating that only numeric characters should be entered.
             self.info_label_text.set("You must enter numbers only; no non-numeric characters.")
+
         # Else if all of the entries have numbers outside of their established ranges.
-        elif not(2 <= int(col_entry)) or not(2 <= int(row_entry)) and not (10 <= int(cell_size_number) <= 150):
+        elif not (2 <= int(col_entry)) or not (2 <= int(row_entry)) and not (10 <= int(cell_size_number) <= 150):
             # display a message stating the condition.
             self.info_label_text.set("The column and row numbers must be within a range of 2 to 31."
                                      "\nThe cell size must be an integer from 10 to 150 (inclusive).")
+
         # Else if the numbers for the columns and rows entries are outside of their bounds and the number
         # in the cell size entry is within bound.
-        elif not(2 <= int(col_entry)) or not(2 <= int(row_entry)):
+        elif not (2 <= int(col_entry)) or not (2 <= int(row_entry)):
             # display a message stating the condition.
             self.info_label_text.set("The column and row numbers must be within a range of 2 to 31.")
+
         # Else if the numbers for the columns and rows entries are within bounds but not the number
         # of the cell size entry.
-        elif not(10 <= int(cell_size_number) <= 150):
+        elif not (10 <= int(cell_size_number) <= 150):
             # display a message stating the condition.
             self.info_label_text.set("The cell size must be an integer from 10 to 150 (inclusive).")
         # Else, this means that the entries passed all the validation tests
         else:
             # hence, clear the validation message as the entries met all of their conditions
             self.info_label_text.set("")
+
+            if cell_size_number == '' or cell_size_number.isdigit():
+                self.suggested_text_val.set(
+                    f"Suggested size: "
+                    f"{Vars.padding_func(max(int(col_entry), int(row_entry)))}")
+
             enable = True  # and raise the flag to enable the button.
 
         # If enable is true
@@ -159,6 +196,9 @@ class TkinterWindow:
         # If the close button is pressed (x button at the top-right corner),
         # close the window.
         raise SystemExit
+
+    def get_wind_size(self):
+        return self.master.winfo_width(), self.master.winfo_height()
 
 
 def start_loop():
