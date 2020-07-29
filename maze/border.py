e85dffdc25db9e65ff66099249cb08a74ca83800
Charl86 <carl.hernan34@gmail.com>
2020-07-29 17:58:44 -0400

    Renamed directory to 'maze'.

---
+++ maze/border.py
@@ -0,0 +1,37 @@
+import pygame
+import pygame.gfxdraw
+from config import Colors, Vars, PygameVars as Pyv
+
+
+class Border:
+    # Takes as arguments an x and y coordinate, that represent the coordinates of the
+    # top-left corner of the cell.
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+        # Corners of the border.
+        # self.top_left_corn = self.x, self.y
+        # self.bot_left_corn = self.x, self.y + Pyv.HEIGHT - 2 * self.y
+        # self.top_right_corn = self.x + Pyv.WIDTH - 2 * self.x, self.y
+        # self.bot_right_corn = self.x + Pyv.WIDTH - 2 * self.x, self.y + Pyv.HEIGHT - 2 * self.y
+
+        # Dimensions of border
+        self.horizon_length = Vars.COLS * Vars.SIZE
+        self.vertical_length = Vars.ROWS * Vars.SIZE
+        # self.horizon_length = Pyv.WIDTH - 2 * self.x
+        # self.vertical_length = Pyv.WIDTH - 2 * self.y
+
+        self.color = Colors.RED  # The color of the border.
+        self.thickness = 2  # The thickness of the border.
+
+    def draw(self):
+        # if self.x or self.y:
+        #     pygame.draw.rect(Pyv.SCREEN, self.color, pygame.Rect(
+        #         (self.x, self.y), (Pyv.WIDTH - 2 * self.x, Pyv.HEIGHT - 2 * self.y)),
+        #                      self.thickness)
+        if self.x or self.y:
+            pygame.draw.rect(Pyv.SCREEN, self.color, pygame.Rect(
+                (self.x, self.y), (self.horizon_length, self.vertical_length)),
+                             self.thickness)
+
