b3bced2be2806c54c8474fc813db6ca6ab1a6889
Charl86 <carl.hernan34@gmail.com>
2020-08-22 16:31:46 -0400

    Changed some comments.

--- mazeGenerator/mazeParts/debugger.py
+++ mazeGenerator/mazeParts/debugger.py
@@ -2,13 +2,15 @@ import logging
 
 
 class Debugger(object):
+    logFile = f"./mazeGenerator/logs/{__name__}.log"
+
     formatter = logging.Formatter("%(message)s")
 
-    # LOGGER = logging.getLogger(__name__)
-    # LOGGER.setLevel(logging.INFO)
-    # file_handler = logging.FileHandler(log_file)
+    # LOGGER = logging.getLogger(logFile)
+    # LOGGER.setLevel(logging.DEBUG)
+    # file_handler = logging.FileHandler(logFile)
     # file_handler.setFormatter(formatter)
-    # file_handler.setLevel(logging.INFO)
+    # file_handler.setLevel(logging.DEBUG)
 
     STREAMER = logging.getLogger(__name__)
     STREAMER.setLevel(logging.DEBUG)
