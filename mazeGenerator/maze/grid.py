14ca6f1bd8e8e1f9f8e56ef2cf2c9971d5e54868
Carlos Hernandez <carl.hernan34@gmail.com>
2020-09-20 03:02:53 -0400

    Moved 2D-Array creation from generator.py to grid.py.

--- mazeGenerator/maze/grid.py
+++ mazeGenerator/maze/grid.py
@@ -1,8 +1,27 @@
+from mazeGenerator.maze.cell import Cell
 
 
 class Grid:
     def __init__(self):
-        self.elements = []
+        self.elements = []  # 2D-Array.
+
+    def populateGrid(self, mSettings):
+        nth_row = []  # Creation of the nth-array to-be-appended to 'the_grid'.
+
+        # For row in number of rows:
+        for row in range(mSettings.rows):
+            for col in range(mSettings.cols):  # For col in number of cols:
+                # Create Cell object with coordinates (col, row) and pass in settings.
+                new_cell = Cell(col, row, mSettings)
+
+                # Append new cell to current row.
+                nth_row.append(new_cell)
+
+            # Append current row to grid.
+            self.elements.append(nth_row)
+
+            # Clear row for next colums iteration.
+            nth_row = []
 
     @property
     def width(self):
